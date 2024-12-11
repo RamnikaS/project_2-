9108e85ec994e6af90f61578a0520724f8b3aba9
Ramnika Saggu <n01520297@humber.ca>
2024-12-10 22:14:38 -0500

    updated the App.js file imported all the components

--- dost-website/src/App.js
+++ dost-website/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Categories from './components/Categories';
+import Services from './components/Services';
+import Reviews from './components/Reviews';
+import Subscribe from './components/Subscribe';
+import Footer from './components/Footer';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <Router>
+    <div className="app">
+      <Header />
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner />
+                <Categories />
+                <Services />
+                <Reviews />
+                <Subscribe />
+              </>
+            }
+          />
+          {/* <Route path="/categories" element={<Categories />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/subscribe" element={<Subscribe />} /> */}
+        </Routes>
+      </main>
+      <Footer />
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;

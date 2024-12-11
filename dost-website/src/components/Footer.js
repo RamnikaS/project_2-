ce42a23bcfc6f3534d75a76e791a8d511c700dde
Ramnika Saggu <n01520297@humber.ca>
2024-12-10 22:07:54 -0500

    Created the footer

---
+++ dost-website/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Footer = () => (
+  <footer>
+    <div >
+      <nav >
+      <Link >About</Link>
+        <Link >Projects</Link>
+        <Link >Services</Link>
+        <Link >Clients</Link>
+        <Link >Blog</Link>
+        <Link >Contact</Link>
+      </nav>
+    </div>
+    <p>&copy; 2024 Dost. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;

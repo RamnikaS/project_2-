072bd648bbf955db55de84415ce11645e880b841
Sukhman Brar <n01726797@humber.ca>
2024-12-11 09:40:59 -0500

    Added State to show /hide service details

--- dost-website/src/components/Services.js
+++ dost-website/src/components/Services.js
@@ -1,39 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Services = () => (
-  <section >
-    <h2 >Our Services</h2>
-    <div>
-      <div >
-        <img
-          src="https://helvik.s3.amazonaws.com/queen-creek/images/pet-pharmacy.jpg"
-          alt="Pharmacy Icon"
-        />
-        <h3>Pharmacy</h3>
-        <p>
-          Providing essential medications to keep your pet healthy and safe
-          from allergies.<br /> ADDITIONAL HELP
-        </p>
-        <a href="https://thepetpharmacist.ca/">
-          Explore
-        </a>
-      </div>
+const Services = () => {
+  const [showPharmacy, setShowPharmacy] = useState(false);
+  const [showGrooming, setShowGrooming] = useState(false);
+
+  return (
+    <section>
+      <h2>Our Services</h2>
       <div>
-        <img
-          src="https://nowtoronto.com/wp-content/uploads/2021/06/Dog-getting-groomed.jpg"
-          alt="Grooming Icon"
-        />
-        <h3>Breed-specific Grooming</h3>
-        <p>
-          Custom haircuts tailored to your pet's breed to prevent skin issues
-          and matting.
-        </p>
-        <a href="https://www.jklgrooming.com/dog_grooming_charts" >
-          Explore
-        </a>
+        <div>
+          <img
+            src="https://helvik.s3.amazonaws.com/queen-creek/images/pet-pharmacy.jpg"
+            alt="Pharmacy Icon"
+          />
+          <h3>Pharmacy</h3>
+          <p>
+            Providing essential medications to keep your pet healthy and safe
+            from allergies.<br /> ADDITIONAL HELP
+          </p>
+          <button onClick={() => setShowPharmacy(!showPharmacy)}>
+            {showPharmacy ? 'Hide Details' : 'Show Details'}
+          </button>
+          {showPharmacy && (
+            <p>Learn more about the pharmacy services we offer...</p>
+          )}
+          <a href="https://thepetpharmacist.ca/">Explore</a>
+        </div>
+        <div>
+          <img
+            src="https://nowtoronto.com/wp-content/uploads/2021/06/Dog-getting-groomed.jpg"
+            alt="Grooming Icon"
+          />
+          <h3>Breed-specific Grooming</h3>
+          <p>
+            Custom haircuts tailored to your pet's breed to prevent skin issues
+            and matting.
+          </p>
+          <button onClick={() => setShowGrooming(!showGrooming)}>
+            {showGrooming ? 'Hide Details' : 'Show Details'}
+          </button>
+          {showGrooming && (
+            <p>Explore grooming options for your pet's breed...</p>
+          )}
+          <a href="https://www.jklgrooming.com/dog_grooming_charts">Explore</a>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Services;
+

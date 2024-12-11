469d3259c4dfec6e97a53d59dee688626d2b25e9
Ramnika Saggu <n01520297@humber.ca>
2024-12-10 22:06:56 -0500

    Created the Subscribe section with an email input with a place holder

---
+++ dost-website/src/components/Subscribe.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Subscribe = () => (
+  <section id="subscribe">
+    <h3>Subscribe & Get Updates</h3>
+    <input type="email" placeholder="Enter your email" />
+  </section>
+);
+
+export default Subscribe;

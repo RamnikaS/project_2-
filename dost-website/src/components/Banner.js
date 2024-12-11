6e836e1383273d5f52ca8192a3b7209b26eb7dd8
Ramnika Saggu <n01520297@humber.ca>
2024-12-10 22:02:15 -0500

    Created the banner component for landing page

---
+++ dost-website/src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Banner = () => (
+  <section >
+    <div>
+      <h2>Available in select states</h2>
+      <h1>
+        A PET-FIRST APPROACH TO WELLNESS <br /> FOR MORE NEWS
+      </h1>
+      <a href="https://globalnews.ca/tag/pet-wellness/" >
+        Learn More
+      </a>
+    </div>
+    <img src="./Dog landing Page-02.jpg" alt="Dog Jasper"/>
+  </section>
+);
+
+export default Banner;

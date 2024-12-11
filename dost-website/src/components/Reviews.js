66f6446a9a0094855694ac6c81d137aa181b0e04
Ramnika Saggu <n01520297@humber.ca>
2024-12-10 22:05:17 -0500

    Created the reviews section with buttons in it

---
+++ dost-website/src/components/Reviews.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Reviews = () => (
+  <section>
+    <div >
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa94mTNTVWxgkIpAv6qFWni_WOmGr4UYZfPw&s"
+        alt="Customer"
+      />
+      <h3>Customer Reviews</h3>
+      <p>
+        "The staff at Pet-First provided excellent advice on pet food. Highly
+        recommended!"
+      </p>
+      <div >
+        <button>&lt;</button>
+        <button>&gt;</button>
+      </div>
+    </div>
+  </section>
+);
+
+export default Reviews;

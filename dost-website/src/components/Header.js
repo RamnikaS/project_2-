a80171fa12cb3f9df72e21b8c926953e4d239ce4
Sukhman Brar <n01726797@humber.ca>
2024-12-11 10:08:35 -0500

    header props

--- dost-website/src/components/Header.js
+++ dost-website/src/components/Header.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <img src="./Dost Logo-02.png" alt="Pet-First Logo" />
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="#">Medicare</Link></li>
-        <li><Link to="#">Blog</Link></li>
-        <li><Link to="#">Sign Up</Link></li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = ({ navLinks }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header>
+      <img src="./Dost Logo-02.png" alt="Pet-First Logo" />
+      <nav>
+        <ul className={isMenuOpen ? 'open' : ''}>
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <Link to={link.path}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+        <button onClick={toggleMenu}>
+          {isMenuOpen ? 'Close Menu' : 'Open Menu'}
+        </button>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
+
 
